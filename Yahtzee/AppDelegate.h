16e6150870fb0ee8f9dcf40a07ddaea1993da87a
Hussachai Puripunpinyo <hussachai[at]gmail[dot]com>
2013-09-08 14:58:46 -0500

    Initial Commit

---
+++ Yahtzee/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Yahtzee
+//
+//  Created by Hussachai Puripunpinyo on 9/8/13.
+//  Copyright (c) 2013 Hussachai Puripunpinyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Yahtzee/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Yahtzee
+//
+//  Created by Hussachai Puripunpinyo on 9/8/13.
+//  Copyright (c) 2013 Hussachai Puripunpinyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
